33359618687de1d30f129c79e8fbf5be9fc7a1f9
Ryan Collins <techrapportrc@gmail.com>
2016-02-24 04:19:27 +0000

    Feat: add the brand report generator

--- lib/app.rb
+++ lib/app.rb
@@ -7,18 +7,6 @@ products_hash = JSON.parse(file)
 date = Time.new.localtime
 puts date
 
-puts "##########################################"
-puts "                     _            _       "
-puts "                    | |          | |      "
-puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
-puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
-puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
-puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
-puts "| |                                       "
-puts "|_|                                       "
-puts 
-puts "##########################################"
-
 # Store the JSON data as an array of items
 items = products_hash["items"]
 
@@ -31,61 +19,98 @@ items = products_hash["items"]
   # Calculate and print the average discount (% or $) based off the average sales price
 
 class Toy
-  attr_accessor :title, :price, :total_purchases, :total_sales, 
-                                        :average_price, :average_discount
+  # Define getter for title
+  attr_accessor :title
   
-  def initialize(item)
-    @title = item["title"]
-    @price = item["full-price"]
-    @purchases = item["purchases"]
-    @total_purchases = 0
-    @average_price = 0.0
-    @total_sales = 0.0
-    @average_discount = 0.0
-    calculate_purchases
-  end
-  
-  
-  def calculate_purchases
-    @purchases.each do |purchase|
-      @total_purchases += 1
-      @total_sales += purchase["price"]
+    # Initialize instance variables for the class
+    def initialize(item)
+      @title = item["title"]
+      @price = item["full-price"]
+      @purchases = item["purchases"]
+      @total_purchases = 0
+      @average_price = 0.0
+      @total_sales = 0.0
+      @average_discount = 0.0
+      @total_discount = 0.0
+      calculate_purchases
     end
-    @average_price = @total_sales / @total_purchases
-  end
   
-end
+    def calculate_average_discount
+      # Only if the average price is less than the price,
+      # Calculate the average discount
+      if @average_price < @price 
+        @average_discount = @price - @average_price
+      end
+    end
+  
+  
+    def calculate_purchases
+      @purchases.each do |purchase|
+        @total_purchases += 1
+        @total_sales += purchase["price"]
+      end
+      @average_price = @total_sales / @total_purchases
+      calculate_average_discount
+    end
+  
+  end
 
-class ReportGenerator
-  def initialize(items)
-    @items = items
+  class ReportGenerator
+    def initialize(items)
+      @items = items
+    end
+    
+
+        puts " _                         _     "
+	      puts "| |                       | |    "
+	      puts "| |__  _ __ __ _ _ __   __| |___ "
+	      puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+	      puts "| |_) | | | (_| | | | | (_| \\__ \\"
+	      puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+	      puts
+	 
+
+	    puts "##########################################"
+      puts "                     _            _       "
+      puts "                    | |          | |      "
+      puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+      puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+      puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+      puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+      puts "| |                                       "
+      puts "|_|                                       "
+      puts 
+      puts "##########################################"
   end
   
-  def print_report
-    @items.each do |item|
-      some_toy = Toy.new(item)
-      puts "#{some_toy.title}"
-      puts "******************************"
-      puts "Retail Price     |    #{some_toy.price}"
-      puts "Total Purchase   |    #{some_toy.total_purchases}"
-      puts "Total Sales      |    $#{some_toy.total_sales}"
-      puts "Average Price    |    $#{some_toy.average_price}"
-      puts "Average Discount |    $#{some_toy.average_discount}"
-      puts "*******************************\r\n\r\n"
+    def print_report
+      #print the product header
+ 
+        print_product_header   
+    # For each item, print the details  
+      @items.each do |item|
+        some_toy = Toy.new(item)
+      
+        # Output the report
+        puts "#{some_toy.title}"
+        puts "******************************"
+        puts "Retail Price     |    $#{some_toy.price}"
+        puts "Total Purchase   |    #{some_toy.total_purchases}"
+        puts "Total Sales      |    $#{some_toy.total_sales}"
+        puts "Average Price    |    $#{some_toy.average_price}"
+        puts "Average Discount |    %#{some_toy.average_discount}"
+        puts "*******************************\r\n\r\n"
+      end
     end
+    
+
+    
   end
-end
 
   Report = ReportGenerator.new(items)
   Report.print_report
   
-	puts " _                         _     "
-	puts "| |                       | |    "
-	puts "| |__  _ __ __ _ _ __   __| |___ "
-	puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
-	puts "| |_) | | | (_| | | | | (_| \\__ \\"
-	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
-	puts
+
 
 # For each brand in the data set:
   # Print the name of the brand
