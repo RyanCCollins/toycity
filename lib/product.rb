832c679b8ade04f7a306b4e5fc26fb9b8f68490e
Ryan Collins <rccproductions@mac.com>
2016-03-01 00:02:51 -0500

    Feat: add product class methods

--- lib/product.rb
+++ lib/product.rb
@@ -3,27 +3,34 @@ require_relative "errors"
 class Product
   @@products = []
   attr_reader :title, :price, :stock
-    
+
   def initialize(options={})
     @title = options[:title]
     @price = options[:price]
     @stock = options[:stock]
     add_to_products
   end
-    
+
   def self.all
     @@products
   end
-    
+
+  def self.in_stock
+    @@products.select { |product| product.in_stock?}
+  end
+
+  def in_stock?
+    @stock > 0
+  end
+
   def find_by_title(title)
     @@products.find {|product| product.title == title}
   end
-  
+
   def remove_stock!(options={})
 
   end
 
-    
   private
 
   def add_to_products
@@ -33,9 +40,9 @@ class Product
       raise DuplicateProductError, "'#{self.title}'' already exists."
     end
   end
-  
+
   def product_exists?
     return find_by_title(self.title)
   end
-  
+
 end
