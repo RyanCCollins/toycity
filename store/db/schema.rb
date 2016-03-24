f757b90102272c781063e89958d37a2e4868d547
Ryan Collins <rccproductions@mac.com>
2016-03-24 09:07:43 -0400

    Fix: alter description name because of incorrect spelling
     * The brands table had a column accidentally spelt desctiption vs. description

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324121048) do
+ActiveRecord::Schema.define(version: 20160324130657) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
     t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   add_index "brands", ["product_id"], name: "index_brands_on_product_id"
