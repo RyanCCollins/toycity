19b6f6cfcb0d98a6aa3c21134dc10ee4ac409e44
Ryan Collins <rccproductions@mac.com>
2016-03-28 17:57:51 -0400

    Fix: for wrong name variable under method missing

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -1,23 +1,29 @@
 class Module
+  # Method missing is called when a method does not exist.
+  # @params method name, array of args and a block
   def method_missing(method_name, *args, &block)
-    name = name.to_s.downcase
+    name = method_name.to_s.downcase
     # Attempt to match any method with the prefix of
-      # find_by_ to try to print out the background color.
-    if(match_data = /^find_by_()(\w*?)?$/.match name)
+      # find_by_
+     if(match_data = /^find_by_()(\w*?)?$/.match name)
     # If a match from the regexp is found to equal find_by_
       # create a finder method for it
       create_finder_methods match_data[2]
       send(method_name, *args)
+    else
+      super(method_name, *args)
     end
   end
+
+  # Creates a finder method for basically any find_by_ + attribute method
+  # @params An array of attributes
+  # @return Product(s) or empty array.
   def create_finder_methods(*attributes)
-    # Your code goes here!
-    # Hint: Remember attr_reader and class_eval
     attributes.each do |attr|
       attr_name = attr.to_s
       method_name = "find_by_#{attr_name}"
       method = %Q{
-        def #{method_name} identifier
+        def #{method_name}(identifier)
           self.all.select{ |product| product.#{attr_name}.to_s == identifier.to_s }
         end
       }
@@ -29,8 +35,4 @@ class Module
   def respond_to_missing?(method_name, include_private = false)
     method_name.to_s.start_with?('find_by_') || super
   end
-end
-
-class Module
-
 end
