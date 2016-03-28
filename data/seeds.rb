baad3909d12d5c48782b81a1c033e1e3ddc02953
Ryan Collins <rccproductions@mac.com>
2016-03-27 20:45:58 -0400

    Feat: add faker seeding to db

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,5 +4,10 @@ require_relative './schema'
 # fake data for testing purposes
 
 def db_seed
-
+  10.times do |i|
+    brand = Faker::Company.name
+    name  = Faker::Commerce.product_name
+    price = Faker::Commerce.price
+    Product.create(brand: brand, name: name, price: price)
+  end
 end
